17a352cc07a874f96c5cf69c1d26feeb3dfa0e00
Srikanth Chekuri <srikanth.chekuri92@gmail.com>
2022-10-18 11:36:54 +0530

    Bump batch size

--- exporter/clickhousemetricsexporter/exporter.go
+++ exporter/clickhousemetricsexporter/exporter.go
@@ -39,7 +39,7 @@ import (
 	"go.opentelemetry.io/collector/pdata/pmetric"
 )
 
-const maxBatchByteSize = 3000000
+const maxBatchByteSize = 90000000
 
 // PrwExporter converts OTLP metrics to Prometheus remote write TimeSeries and sends them to a remote endpoint.
 type PrwExporter struct {
